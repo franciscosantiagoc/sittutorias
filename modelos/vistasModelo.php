fdb2c9ed1e49e2cee4890500bf069a71d852a94c
Francisco Santiago de la Cruz <francisco.santiago@itistmo.edu.mx>
2020-07-28 15:29:49 -0500

    trabajando en vistas y enlaces

--- modelos/vistasModelo.php
+++ modelos/vistasModelo.php
@@ -4,11 +4,7 @@
         
         /*-------------- Modelo obtener vistas --------------*/
         protected static function obtener_vistas_modelo($vistas){
-<<<<<<< HEAD
-            $listaBlanca=["home","Registro","MenuAlumno","MenuCordArea","MenuCordCa","MenuRoot","MenuTutor","login", "CCActividades","CCAsignTutorado","CCEdit-perfil","CCEstadisticos","CCNotification","CCoordinadores","CCSolicitudes","CCTutorados","CCTutores","CoordinadoresAR","CoordinadoresCR","RootEstadisticosR","RootCoordinadoresAR","RootCoordinadoresCR"];
-=======
-            $listaBlanca=["home","Registro","MenuAlumno","MenuCordArea","MenuCa","MenuRoot","login", "CCActividades","CCAsignTutorado","CCEdit-perfil","CCEstadisticos","CCNotification","CCoordinadores","CCSolicitudes","CCTutorados","CCTutores","CoordinadoresAR","CoordinadoresCR","EstadisticosR","AlumnAct","AlumnEditPerfil","AlumnInfo","AlumnNotif","AlumnSolic","TutorAct","TutorEditPerfil","TutorInfo","TutorNotif","TutorTutorados"];
->>>>>>> 15104936ff13ee6a2a494ef2c4e30bb85fd0493c
+			$listaBlanca=["home","Registro","MenuAlumno","MenuCordArea","MenuCordCa","MenuRoot","MenuTutor","login", "CCActividades","CCAsignTutorado","CCEdit-perfil","CCEstadisticos","CCNotification","CCoordinadores","CCSolicitudes","CCTutorados","CCTutores","CoordinadoresAR","CoordinadoresCR","RootEstadisticosR","RootCoordinadoresAR","RootCoordinadoresCR","AlumnAct","AlumnEditPerfil","AlumnInfo","AlumnNotif","AlumnSolic","TutorAct","TutorEditPerfil","TutorInfo","TutorNotif","TutorTutorados"];
 			if(in_array($vistas, $listaBlanca)){
 				if(is_file("./vistas/contenidos/".$vistas."-view.php")){
 					$contenido="./vistas/contenidos/".$vistas."-view.php";
