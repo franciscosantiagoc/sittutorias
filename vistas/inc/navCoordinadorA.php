6092a3d244a684d016b7b70f794ca9db52847c87
Juan carlos fernandez <fernandez34456@gmail.com>
2020-07-28 21:27:40 -0500

    Enlazando paginas en Coordinador area y Carrera

--- vistas/inc/navCoordinadorA.php
+++ vistas/inc/navCoordinadorA.php
@@ -9,20 +9,20 @@
                 </li>
                 <li class="nav-item dropdown menu"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">Gestion</a>
                     <div class="dropdown-menu" role="menu">
-                        <a class="dropdown-item" role="presentation" href="CordCarrera/CoordinadoresCA.html">Gestión de Coordinadores</a>
-                        <a class="dropdown-item" role="presentation" href="CordCarrera/TutoresC.html">Gestión de Tutores</a><a class="dropdown-item"
-                            role="presentation" href="CordCarrera/TutoradosC.html">Gestión de Tutorados</a>
-                            <a class="dropdown-item" role="presentation" href="CordCarrera/Solicitudes.html">Gestión de Solicitudes</a>
-                            <a class="dropdown-item" role="presentation" href="CordCarrera/ActividadesC.html">Gestión de Actividades</a>
+                        <a class="dropdown-item" role="presentation" href="<?php echo SERVERURL;?>CCoordinadores">Gestión de Coordinadores</a>
+                        <a class="dropdown-item" role="presentation" href="<?php echo SERVERURL;?>CCTutores">Gestión de Tutores</a><a class="dropdown-item"
+                            role="presentation" href="<?php echo SERVERURL;?>CCTutorados">Gestión de Tutorados</a>
+                            <a class="dropdown-item" role="presentation" href="<?php echo SERVERURL;?>CCSolicitudes">Gestión de Solicitudes</a>
+                            <a class="dropdown-item" role="presentation" href="<?php echo SERVERURL;?>CCActividades">Gestión de Actividades</a>
                     </div>
                 </li>
-                <li class="nav-item menu" role="presentation"><a class="nav-link js-scroll-trigger" href="">Estadísticas</a></li>
+                <li class="nav-item menu" role="presentation"><a class="nav-link js-scroll-trigger" href="<?php echo SERVERURL;?>CCEstadisticos">Estadísticas</a></li>
                 <li class="dropdown menu">
                     <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false" id="div-img-perfil"><img class="rounded-circle" id="img-perfil" src="vistas/assets/img/coor-ivan.jpg"></a>
                     <div class="dropdown-menu" role="menu">
                         <a class="dropdown-item disabled" role="presentation">Iván Ruiz Sánchez</a>
-                        <a class="dropdown-item" role="presentation" href="CordCarrera/edit-perfilC.html">Editar Perfil</a>
-                        <a class="dropdown-item" role="presentation" href="Cerrar Sesión">Cerrar Sesión</a></div>
+                        <a class="dropdown-item" role="presentation" href="<?php echo SERVERURL;?>CCEditar-perfil">Editar Perfil</a>
+                        <a class="dropdown-item" role="presentation" href="<?php echo SERVERURL;?>login">Cerrar Sesión</a></div>
                 </li>
             </ul>
             
--- vistas/inc/navCoordinadorC.php
+++ vistas/inc/navCoordinadorC.php
@@ -6,32 +6,32 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="nav navbar-nav ml-auto">
                 <li class="nav-item menu" role="presentation">
-                    <a class="nav-link js-scroll-trigger" href="Alumno.html">Inicio</a>
+                <a class="nav-link js-scroll-trigger" href="<?php echo SERVERURL;?>MenuCordCa">Inicio</a>
                 </li>
                 <li class="nav-item dropdown menu">
                     <a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">Gestion</a>
                     <div class="dropdown-menu" role="menu">
-                        <a class="dropdown-item" role="presentation" href="CordCarrera/TutoresC.html">Gestión de Tutores</a>
-                        <a class="dropdown-item" role="presentation" href="CordCarrera/TutoradosC.html">Gestión de Tutorados</a>
-                        <a class="dropdown-item" role="presentation" href="CordCarrera/ActividadesC.html">Gestión de Actividades</a>
+                        <a class="dropdown-item" role="presentation" href="<?php echo SERVERURL;?>CCTutores">Gestión de Tutores</a>
+                        <a class="dropdown-item" role="presentation" href="<?php echo SERVERURL;?>CCTutorados">Gestión de Tutorados</a>
+                        <a class="dropdown-item" role="presentation" href="<?php echo SERVERURL;?>CCActividades">Gestión de Actividades</a>
                     </div>
                 </li>
                 <li class="nav-item dropdown show menu">
-                    <a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false">Tutor</a>
+                    <a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">Tutor</a>
                     <div class="dropdown-menu" role="menu">
-                        <a class="dropdown-item" role="presentation" href="VistaTutores/Tutorados.html">Gestión de Tutorados</a>
-                        <a class="dropdown-item" role="presentation" href="VistaTutores/ActividadesT.html">Gestión de Actividades</a>
+                        <a class="dropdown-item" role="presentation" href="<?php echo SERVERURL;?>CCTutorados">Gestión de Tutorados</a>
+                        <a class="dropdown-item" role="presentation" href="<?php echo SERVERURL;?>CCActividades">Gestión de Actividades</a>
                     </div>
                 </li>
                 <li class="nav-item menu" role="presentation">
-                    <a class="nav-link js-scroll-trigger" href="">Estadisticas</a></li>
+                    <a class="nav-link js-scroll-trigger" href="<?php echo SERVERURL;?>CCEstadisticos">Estadisticas</a></li>
                 <li class="nav-item dropdown menu">
                     <a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" id="div-img-perfil">
                         <img class="rounded-circle" id="img-perfil" src="vistas/assets/img/coor-maribel.jpg"></a>
                     <div class="dropdown-menu" role="menu">
                         <a class="dropdown-item disabled" role="presentation">Maribel Castillejos Toledo</a>
-                        <a class="dropdown-item" role="presentation" href="CordCarrera/edit-perfilC.html">Editar Perfil</a>
-                        <a class="dropdown-item" role="presentation" href="Cerrar Sesión">Cerrar Sesión</a>
+                        <a class="dropdown-item" role="presentation" href="<?php echo SERVERURL;?>CCEdit-perfil">Editar Perfil</a>
+                        <a class="dropdown-item" role="presentation" href="<?php echo SERVERURL;?>login">Cerrar Sesión</a>
                     </div>
                 </li>
             </ul>
