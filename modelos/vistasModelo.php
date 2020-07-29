dee9cda85461327e95a2b8783187be84397ed95e
Juan carlos fernandez <fernandez34456@gmail.com>
2020-07-28 21:52:33 -0500

    Arreglando MenuCordArea MenuCordCa

--- modelos/vistasModelo.php
+++ modelos/vistasModelo.php
@@ -4,7 +4,7 @@
         
         /*-------------- Modelo obtener vistas --------------*/
         protected static function obtener_vistas_modelo($vistas){
-			$listaBlanca=["home","Registro","MenuAlumno","MenuCordArea","MenuCordCa","MenuRoot","MenuTutor","login", "CCActividades","CCAsignTutorado","CCEdit-perfil","CCEstadisticos","CCNotification","CCoordinadores","CCSolicitudes","CCTutorados","CCTutores","CoordinadoresAR","CoordinadoresCR","RootEstadisticosR","RootCoordinadoresAR","RootCoordinadoresCR","AlumnAct","AlumnEditPerfil","AlumnInfo","AlumnNotif","AlumnSolic","TutorAct","TutorEditPerfil","TutorInfo","TutorNotif","TutorTutorados"];
+			$listaBlanca=["home","Registro","MenuAlumno","MenuCordArea","MenuCordCa","MenuRoot","MenuTutor","login", "CCActividades","CCAsignTutorado","CCEdit-perfil","CCEstadisticos","CCNotification","CCoordinadores","CCSolicitudes","CCTutorados","CCTutores","CCInfo","CoordinadoresAR","CoordinadoresCR","RootEstadisticosR","RootCoordinadoresAR","RootCoordinadoresCR","AlumnAct","AlumnEditPerfil","AlumnInfo","AlumnNotif","AlumnSolic","TutorAct","TutorEditPerfil","TutorInfo","TutorNotif","TutorTutorados"];
 			if(in_array($vistas, $listaBlanca)){
 				if(is_file("./vistas/contenidos/".$vistas."-view.php")){
 					$contenido="./vistas/contenidos/".$vistas."-view.php";
---
+++ vistas/contenidos/CCInfo-view.php
@@ -0,0 +1,59 @@
+
+<?php include "./vistas/inc/navTutor.php"; ?>
+    <div class="register-photo">
+        <div id="importcsvregis" class="form-container">
+            <div class="form-container">
+                <form method="post">
+                    <h2 class="text-center"><strong>Información</strong></h2><div class="team-boxed">
+    <div class="container">
+        <div class="intro">
+            <h2 class="text-center">Tutores </h2>
+        </div>
+        <div class="row people">
+            <div class="col-md-6 col-lg-4 item">
+                <div class="box"><img class="rounded-circle" src="./vistas/assets/img/1.jpg" />
+                    <h3 class="name">Alberto Ramírez Regalado</h3>
+                    <b>Area: </b><p class="description">Sistemas e informática</p>
+                    <b>Matrícula: </b><p class="description">25635453</p>
+                    <div class="enlaces"><a href="#">Ver</a><a class="edit" href="#">Editar</a></div> 
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4 item">
+                <div class="box"><img class="rounded-circle" src="./vistas/assets/img/2.jpg" />
+                    <h3 class="name">Maribel Castillejos Toledo</h3>
+                    <b>Area: </b><p class="description">Sistemas e informática</p>
+                    <b>Matrícula: </b><p class="description">25635453</p>
+                    <div class="enlaces"><a href="#">Ver</a><a class="edit" href="#">Editar</a></div>  
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4 item">
+                <div class="box"><img class="rounded-circle" src="./vistas/assets/img/3.jpg" />
+                    <h3 class="name">Angel Olivarez Perez</h3>
+                    <b>Area: </b><p class="description">Sistemas e informática</p>
+                    <b>Matrícula: </b><p class="description">25635453</p>
+                    <div class="enlaces"><a href="#">Ver</a><a class="edit" href="#">Editar</a></div>      
+                </div>
+            </div>
+        </div>
+    </div>
+</div></form>
+            </div>
+            <div id="dat-coord" class="form-container">
+                <form method="post">
+                    <div class="form-group">
+                        <p><strong>Coordinador de Área</strong></p>
+                    </div><img id="imgreg" src="./vistas/assets/img/alum3.jpg">
+                    <div class="form-group"><input class="form-control" type="text" placeholder="Nombre" name="name"></div>
+                    <div class="form-group"><input class="form-control" type="text" placeholder="Apellido Paterno"></div>
+                    <div class="form-group"><input class="form-control" type="text" placeholder="Apellido Materno"></div>
+                    <div class="form-group"><select class="form-control"><option value="" selected="">Sexo</option><option value="1">Hombre</option><option value="2">Mujer</option></select></div>
+                    <div class="form-group"><input class="form-control" type="tel" placeholder="Número de Telefono"></div>
+                    <div class="form-group"><input class="form-control" type="email" name="email" placeholder="Email"></div>
+                    <div class="form-group"><select class="form-control"><option value="" selected="">Carrera</option><option value="13">Arquitectura</option><option value="14">Informatica</option><option value="15">Ingenieria Civil</option><option value="16">Ingenieria en Sistemas Computacionales</option></select></div>
+                    <div
+                        class="form-group"><input class="form-control" type="text" placeholder="Departamento"></div>
+            <div class="form-group"><input class="form-control" type="text" placeholder="No. Cubiculo"></div>
+            </form>
+        </div>
+    </div>
+    </div>
