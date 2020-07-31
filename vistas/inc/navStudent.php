c54638693e99686ec86d5ed2b91d7a67a2a15705
Luis Alberto <alberto.robles@itistmo.edu.mx>
2020-07-30 21:09:16 -0500

    Quitando comentarios

--- vistas/inc/navStudent.php
+++ vistas/inc/navStudent.php
@@ -1,28 +1,4 @@
 
-<!--<nav class="navbar navbar-light navbar-expand-lg fixed-top bg-primary" id="mainNav">
-    <div class="container"><a class="navbar-brand js-scroll-trigger" id="tit_pag" href="#page-top">Tutorias</a><button data-toggle="collapse" data-target="#navbarResponsive" class="navbar-toggler navbar-toggler-right" type="button" aria-controls="navbarResponsive" aria-expanded="false"
-            aria-label="Toggle navigation"><i class="fa fa-align-justify"></i></button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="nav navbar-nav ml-auto">
-                <li class="nav-item menu" role="presentation"><a class="nav-link js-scroll-trigger" href="<?php echo SERVERURL;?>MenuAlumno">Inicio</a></li>
-                <li class="nav-item menu" role="presentation"><a class="nav-link js-scroll-trigger" href="<?php echo SERVERURL;?>AlumnAct">Actividades</a></li>
-                <li class="nav-item menu" role="presentation"><a class="nav-link js-scroll-trigger" href="<?php echo SERVERURL;?>AlumnSolic">Solicitudes</a></li>
-                <li class="nav-item menu" role="presentation"><a class="nav-link js-scroll-trigger" href="<?php echo SERVERURL;?>AlumnInfo">Contacto</a></li>
-                <li class="nav-item menu" role="presentation"><a class="nav-link js-scroll-trigger" href="<?php echo SERVERURL;?>AlumnNotif">Notificaciones</a></li>
-                <li class="nav-item dropdown">
-                    <a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" id="div-img-perfil">
-                        <img class="rounded-circle" id="img-perfil" src="./vistas/assets/img/alum5.jpg"></a>
-                    <div class="dropdown-menu" role="menu">
-                        <a class="dropdown-item disabled" role="presentation">Humberto Toledo Fuentes</a>
-                        <a class="dropdown-item" role="presentation" href="AlumnEditPerfil">Editar Perfil</a>
-                        <a class="dropdown-item" role="presentation" href="login">Cerrar Sesión</a>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>-->
-
 <nav class="navbar navbar-light navbar-expand-md navbar-side" id="nav-menu">
         <div class="container-fluid">
             <div id="div-mobilemenu"><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1" id="icon-menu"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button></div><a class="navbar-brand d-sm-flex" href="" style="color: #ffffffff;">Sistema de Tutorias Itistmo</a>
