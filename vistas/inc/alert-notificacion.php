b6e7439edb58bf7d0ee27621020a65f4caf3e770
Francisco Santiago de la Cruz <francisco.santiago@itistmo.edu.mx>
2020-07-30 19:45:59 -0500

    alert-notificacion

---
+++ vistas/inc/alert-notificacion.php
@@ -0,0 +1,5 @@
+<div class="container" id="div-container-msj">
+    <div class="col" id="div-msj-notif">
+        <div class="alert alert-success" role="alert"><span><strong>Mensajes Nuevos en su Bandeja de entrada.</strong><br></span><button class="btn btn-primary" id="btn-cerrar-msj" type="button">Cerrar</button></div>
+    </div>
+</div>
