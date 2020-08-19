a09805e655f8978e2e90a8c874fde306fab08527
Luis Alberto <alberto.robles@itistmo.edu.mx>
2020-08-18 23:25:26 -0500

    Integridad de datos - usuarioController

--- controladores/usuarioController.php
+++ controladores/usuarioController.php
@@ -23,17 +23,91 @@
 
           /* == comprobar campos vacíos ==*/
 
-          if($nombre=="" || $apellido_paterno=="" || $apellido_materno=="" || $numero_telefono=="" || $direccion==""  || $noctrl==""){
+          if($nombre=="" || $apellido_paterno=="" || $apellido_materno=="" || $direccion==""  || $noctrl==""){
               $alerta=[
                   "Alerta"=>"simple",
                   "Titulo"=>"Ocurrio un error inesperado",
                   "Texto"=>"No has llenado todos los campos que son obligatorios",
-                  "Tipo"=>"error",
+                  "Tipo"=>"error"
               ];
               echo json_encode($alerta);
               exit();
 
           }
+
+          /* == Verificando integridad de los datos ==*/
+
+          if(mainModel::verificar_datos("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{1,35}",$nombre)){
+            $alerta=[
+               "Alerta"=>"simple",
+               "Titulo"=>"Ocurrio un error inesperado",
+               "Texto"=>"El NOMBRE no coincide con el formato solicitado",
+               "Tipo"=>"error"
+           ];
+           echo json_encode($alerta);
+           exit();
+
+          }
+          if(mainModel::verificar_datos("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{1,35}",$apellido_paterno)){
+            $alerta=[
+               "Alerta"=>"simple",
+               "Titulo"=>"Ocurrio un error inesperado",
+               "Texto"=>"El APELLIDO PATERNO no coincide con el formato solicitado",
+               "Tipo"=>"error"
+           ];
+           echo json_encode($alerta);
+           exit();
+
+          }
+          if(mainModel::verificar_datos("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{1,35}",$apellido_materno)){
+            $alerta=[
+               "Alerta"=>"simple",
+               "Titulo"=>"Ocurrio un error inesperado",
+               "Texto"=>"El APELLIDO MATERNO no coincide con el formato solicitado",
+               "Tipo"=>"error"
+           ];
+           echo json_encode($alerta);
+           exit();
+
+          }
+          if($numero_telefono!=""){
+            if(mainModel::verificar_datos("[0-9()+]{8,20}",$numero_telefono)){
+               $alerta=[
+                  "Alerta"=>"simple",
+                  "Titulo"=>"Ocurrio un error inesperado",
+                  "Texto"=>"El TELÉFONO no coincide con el formato solicitado",
+                  "Tipo"=>"error"
+              ];
+              echo json_encode($alerta);
+              exit();
+   
+             }
+          }
+
+          if(mainModel::verificar_datos("[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ().,#\- ]{1,190}",$direccion)){
+            $alerta=[
+               "Alerta"=>"simple",
+               "Titulo"=>"Ocurrio un error inesperado",
+               "Texto"=>"La DIRECCION no coincide con el formato solicitado",
+               "Tipo"=>"error"
+           ];
+           echo json_encode($alerta);
+           exit();
+
+          }
+
+          if(mainModel::verificar_datos("[0-9-]{8,10}",$noctrl)){
+            $alerta=[
+               "Alerta"=>"simple",
+               "Titulo"=>"Ocurrio un error inesperado",
+               "Texto"=>"El NUMERO DE CONTROL no coincide con el formato solicitado",
+               "Tipo"=>"error"
+           ];
+           echo json_encode($alerta);
+           exit();
+
+          }
+
 
 
       }
