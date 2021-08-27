43916d117836abb75d0fe35fbf55ae7b42004c1c
MasterFSC <44105867+MasterFSC@users.noreply.github.com>
2021-08-27 02:32:26 -0500

    añadiendo script actualizado de bd

--- config/APP.php
+++ config/APP.php
@@ -4,8 +4,8 @@
 	const COMPANY="Sistema de Gestión de Tutorias";*/
 	
 	if( !defined( 'SERVERURL' ) ) {
-        /*define( 'SERVERURL', "http://localhost:8081/tutorias/" );*/
-       define( 'SERVERURL', "https://sit-itistmo.website/" );
+        define( 'SERVERURL', "http://localhost:8081/tutorias/" );
+       /*define( 'SERVERURL', "https://sit-itistmo.website/" );*/
 	}
 
 	if( !defined( 'COMPANY' ) ) { 
